18b18d68c1718bb29b161244dbe711e644e1b4c7
Umer Bin Ubaid <138484036+workbyumer@users.noreply.github.com>
2025-10-06 17:28:12 +0500

    Add files via upload

---
+++ (components)/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className='flex justify-between p-4 border-b'>
+      <div>Umer Bin Ubaid</div>
+      <div>Links to Portfolios</div>
+    </div>
+  );
+};
+
+export default Navbar;
